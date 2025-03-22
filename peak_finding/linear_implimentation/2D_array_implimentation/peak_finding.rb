fd6d8da340faaa9df224ad64eb88932302c1ef81
zondiMaqina <zondimaqina0@gmail.com>
2025-03-22 13:20:24 +0200

    create method for finding peak in middle array of matrix

--- peak_finding/linear_implimentation/2D_array_implimentation/peak_finding.rb
+++ peak_finding/linear_implimentation/2D_array_implimentation/peak_finding.rb
@@ -8,4 +8,23 @@ class MatrixPeakFinding
     @peak = nil
     @index = []
   end
+
+  # finds peak in middle array of matrix
+  def find_mid_peak(mid_array)
+    start = 0
+    endpoint = mid_array.size
+    while start < endpoint
+      mid = (start + endpoint) / 2
+      if mid_array[mid] < mid_array[mid + 1].to_i
+        start = (mid + 1 + endpoint) / 2
+        endpoint = mid_array.size
+        next
+      elsif mid_array[mid] < mid_array[mid - 1].to_i
+        start = 0
+        endpoint = mid - 1
+        next
+      end
+      return mid_array[mid]
+    end
+  end
 end
--- spec/peak_finding/linear_implimentation/2D_array_implimentation_spec.rb
+++ spec/peak_finding/linear_implimentation/2D_array_implimentation_spec.rb
@@ -5,13 +5,20 @@ require_relative '../../../peak_finding/linear_implimentation/2D_array_impliment
 
 RSpec.describe MatrixPeakFinding do
   let(:matrix) { [[10, 20, 30], [40, 50, 60], [70, 80, 90]] }
-  subject(:peak) { dscribed_class.new }
+  subject(:peak) { described_class.new }
 
-  describe '#find_mid' do
+  describe '#find_mid_peak' do
     context 'when given the midpoint of matrix' do
-      it 'will find midpoint of that array' do
-        midpoint = peak.find_mid
-        expect(midpoint).to eql(50)
+      it 'will find peak of that array' do
+        midpeak = peak.find_mid_peak([40, 50, 60, 80])
+        expect(midpeak).to eql(80)
+      end
+    end
+
+    context 'when peak in mid array is at first index' do
+      it 'will return that element in 1st index' do
+        midpeak = peak.find_mid_peak([80, 40, 30, 20])
+        expect(midpeak).to eql(80)
       end
     end
   end
